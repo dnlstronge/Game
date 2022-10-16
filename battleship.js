3804fcc2a891d00ae85c4cf42146a833c9862969
Dan Stronge <dnlstronge@gmail.com>
2022-10-16 10:04:01 +0100

    Create: add controller obj, moved test code to gametests.js

--- battleship.js
+++ battleship.js
@@ -1,6 +1,6 @@
 //Game objects:
 
-//VIEW
+//VIEW (display)
 
 var view = {
     displayMessage: function(msg) {
@@ -24,14 +24,6 @@ var view = {
 };
 
 
-/* TEST
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMiss("Testing, one... two...test") */
 
 //MODEL: games-state data
 
@@ -82,18 +74,14 @@ var model = {
         }
         return true;
     }
+}
+var controller = {
+        guesses: 0,
+        processGuess: function(guess) {
+            //code to follow
+        }
+
 };
 
-/* test whether game state updates display
 
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
-model.fire("34");
-model.fire("24");
-model.fire("44");
-model.fire("12");
-model.fire("11");
-model.fire("10"); */
 
