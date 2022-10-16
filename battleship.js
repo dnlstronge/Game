094cb0d2410fbd5fd5da82c9979ee529b668118d
Dan Stronge <dnlstronge@gmail.com>
2022-10-16 19:04:04 +0100

    create: add collision function

--- battleship.js
+++ battleship.js
@@ -89,7 +89,7 @@ var model = {
         var col;
         if (direction === 1) {
             row = Math.floor(Math.random() * this.boardSize);
-            col = Math.floor(Math.random()* (this.boardSize - (this.shipLength + 1)));
+            col = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
         } else {
             row = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
             col = Math.floor(Math.random() * this.boardSize);
@@ -104,8 +104,18 @@ var model = {
         }
       }
         return newShipsLocations;  
-    }
-    
+    },
+    collision: function(locations) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            for (var j = 0; j < this.locations.length; j++) {
+            if (ship.locations.indexOf(locations[j]) >= 0) {
+                return true;
+            }
+        }
+      } 
+      return false;
+    } 
 };
 
 // GAME CONTROLLER - player guesses etc
