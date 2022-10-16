5bb661792a1d8f92782255bd95c512565d17a5d9
Dan Stronge <dnlstronge@gmail.com>
2022-10-16 09:17:33 +0100

    create: add method to determine if ship is sunk

--- battleship.js
+++ battleship.js
@@ -33,7 +33,7 @@ view.displayMiss("25");
 view.displayHit("26");
 view.displayMiss("Testing, one... two...test") */
 
-//MODEL: information to pass on to view
+//MODEL: games-state data
 
 var model = {
     boardSize: 7,
@@ -52,15 +52,29 @@ var model = {
     fire: function(guess) {
         for (var i = 0; i  < this.numShips; i++) {
             var ship = this.ships[i];
-            var locations = ship.locations;
-            var index = locations.indexOf(guess);
+            var index = ship.locations.indexOf(guess);
         if (index >= 0) {
             ship.hits[index] = "hit";
+            view.displayHit(guess);
+            view.displayMessage("HIT!")
+            if (this.isSunk(ship)) {
+                view.displayMessage("You sank my battleship!")
+                this.shipsSunk++;
+            }
             return true;
-        }
+          }
         }
         return false;
+    },
+
+          // method to determine if ship is sunk
+     isSunk: function(ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+
+        }
+        return true;
     }
-};
-
-
+}
